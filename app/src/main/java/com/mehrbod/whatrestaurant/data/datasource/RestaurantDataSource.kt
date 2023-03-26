f0b076ac98a89aea627967c3701c0936f2cce503
Mehrbod <mehrbod@ridedott.com>
2023-03-26 15:56:25 +0200

    chore: add comment

--- app/src/main/java/com/mehrbod/whatrestaurant/data/datasource/RestaurantDataSource.kt
+++ app/src/main/java/com/mehrbod/whatrestaurant/data/datasource/RestaurantDataSource.kt
@@ -3,6 +3,11 @@ package com.mehrbod.whatrestaurant.data.datasource
 import com.mehrbod.whatrestaurant.data.datasource.model.RestaurantsResponse
 
 
+/**
+ * This interface is unnecessary for this project, but it's here to demonstrate the need for
+ * an abstraction layer to have different data source, and easily implement other functionalities
+ * related to them, such as offline first, cache, local storage, etc.
+ */
 interface RestaurantDataSource {
     suspend fun getRestaurants(postcode: String): RestaurantsResponse
 }
